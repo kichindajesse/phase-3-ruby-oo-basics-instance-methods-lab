1481ea6409798708de5e406ef5a4f05d06cffb4a
kichinda <53686544+kichinda@users.noreply.github.com>
2023-02-09 14:36:55 +0300

    completed

--- spec/instance_methods_spec.rb
+++ spec/instance_methods_spec.rb
@@ -69,3 +69,29 @@ describe 'Person' do
     end
   end
 end
+
+class Dog
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+
+fido = Dog.new
+fido.bark 
+fido.sit
+
+
+class Person
+  def talk
+    puts "Hello World!"
+  end
+
+  def walk
+    puts "The Person is walking"
+  end
+end
+
